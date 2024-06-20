2de135c355602761ac90e4146c3bda5c1be2cfab
zueunjoeng <judms0314@gmail.com>
2024-06-20 12:54:15 +0900

    [2.1.4] 상단네비게이션 DB연동

--- js/wayo.js
+++ js/wayo.js
@@ -1,113 +1,204 @@
 const d1gnb = [
-       
-                {
-                    text:"펫시터가와요",
-                    href:"/petopia/petsitter.html",
-                    d2_li:[
-                        {
-                            text :"방문돌봄",
-                            href :"/petopia/petsitter.html"
-                        },
-                        {
-                            text :"집중호스피스",
-                            href :"/petopia/pethos.html"
-                        },
-                        {
-                            text :"펫 플래닛",
-                            href :"/petopia/petplan.html"
-                        },
-                        {
-                            text :"웨딩케어",
-                            href :"/petopia/petwedding.html"
-                        },
-                        {
-                            text :"안심&픽업드랍",
-                            href :"/petopia/petpick.html"
-                        },
-                    ],
-                },
-                {
-                    text:"훈련사",
-                    href:"/petopia/petrain.html",
-                    d2_li:[
-                        {
-                            text :"훈련사 소개",
-                            href :"/petopia/petrain.html"
-                        },
-                        {
-                            text :"훈련사에게 물어봐",
-                            href :"/petopia/petask.html"
-                        },
-                    ],
-
-                },
-                {
-                    text:"플레이스",
-                    href:"/petopia/petshop.html",
-                    d2_li:[
-                        {
-                            text :"입점 문의",
-                            href :"/petopia/petshop.html"
-                        },
-                        {
-                            text :"매장 찾기",
-                            href :"/petopia/petplace.html"
-                        },
-                    ],
-
-                },
-                {
-                    text:"사회공헌",
-                    href:"/petopia/petvolunt.html",
-                    d2_li:[
-                        {
-                            text :"봉사활동",
-                            href :"/petopia/petvolunt.html"
-                        },
-                        {
-                            text :"지식 나눔",
-                            href :"/petopia/petknowl.html"
-                        },
-                        {
-                            text :"심리 상담",
-                            href :"/petopia/petsocial.html"
-                        },
-                    ],
-                    
-                },
-                {
-                    text:"커뮤니티",
-                    href:"/petopia/petreview.html",
-                    d2_li:[
-                        {
-                            text :"실시간 후기",
-                            href :"/petopia/petreview.html"
-                        },
-                        {
-                            text :"구독 서비스",
-                            href :"/petopia/petservice.html"
-                        },
-                    ],
-
-                },
-                {
-                    text:"펫트너",
-                    href:"/petopia/petcare.html",
-                    d2_li:[
-                        {
-                            text :"펫시터 지원",
-                            href :"/petopia/petcare.html"
-                        },
-                        {
-                            text :"훈련사 지원",
-                            href :"/petopia/pettraincare.html"
-                        },
-                        {
-                            text :"산책 케어 지원",
-                            href :"/petopia/petwalkcare.html"
-                        },
-                    ],
-                },
-        ] 
-
-
+    {
+        "text": "펫시터가 와요",
+        "href": "/petsitter.html",
+        "cls" : {  
+                    "li" : " d1_li position-relative ", 
+                    "a" :"d1_a",
+                    "ul" : " d2 position-absolute ps-0 "
+         },      
+        "d2_li": [
+            {
+                "text": "방문 돌봄",
+                "href": "/homecare.html",
+                "cls" : {  
+                    "li" : "d2_li", 
+                    "a" :""
+                }
+            },
+            {
+                "text": "집중 호스피스",
+                "href": "/pethos.html",
+                "cls" : {  
+                    "li" : "d2_li", 
+                    "a" :""
+                }
+            },
+            {
+                "text": "펫 플래닛",
+                "href": "/longcare.html",
+                "cls" : {  
+                    "li" : "d2_li", 
+                    "a" :""
+                }
+            },
+            {
+                "text": "웨딩케어",
+                "href": "/weddingcare.html",
+                "cls" : {  
+                    "li" : "d2_li", 
+                    "a" :""
+                }
+            },
+            {
+                "text": "안심 픽업 & 드랍",
+                "href": "/pickupndrop.html",
+                "cls" : {  
+                    "li" : "d2_li", 
+                    "a" :""
+                }
+            }
+        ]
+    },
+    {
+        "text": "훈련사",
+        "href": "/petrainer.html",
+        "cls" : {  
+            "li" : " d1_li position-relative ", 
+            "a" :"d1_a",
+            "ul" : " d2 position-absolute ps-0 "
+ },      
+        "d2_li": [
+            {
+                "text": "훈련사 소개",
+                "href": "/whopetrainer.html",
+                "cls" : {  
+                    "li" : "d2_li", 
+                    "a" :""
+                }
+            },
+            {
+                "text": "훈련사에게 물어봐",
+                "href": "/qnapetrainer.html",
+                "cls" : {  
+                    "li" : "d2_li", 
+                    "a" :""
+                }
+            }
+        ]
+    },
+    {
+        "text": "플레이스",
+        "href": "/petplace.html",
+        "cls" : {  
+            "li" : " d1_li position-relative ", 
+            "a" :"d1_a",
+            "ul" : " d2 position-absolute ps-0 "
+ },      
+        "d2_li": [
+            {
+                "text": "입점 문의",
+                "href": "/askpartnership.html",
+                "cls" : {  
+                    "li" : "d2_li", 
+                    "a" :""
+                }
+            },
+            {
+                "text": "매장 찾기",
+                "href": "/findshop.html",
+                "cls" : {  
+                    "li" : "d2_li", 
+                    "a" :""
+                }
+            }
+        ]
+    },
+    {
+        "text": "사회공헌",
+        "href": "/petsocial.html",
+        "cls" : {  
+            "li" : " d1_li position-relative ", 
+            "a" :"d1_a",
+            "ul" : " d2 position-absolute ps-0 "
+ },      
+        "d2_li": [
+            {
+                "text": "봉사활동",
+                "href": "/volunteer.html",
+                "cls" : {  
+                    "li" : "d2_li", 
+                    "a" :""
+                }
+            },
+            {
+                "text": "지식나눔",
+                "href": "/sharing.html",
+                "cls" : {  
+                    "li" : "d2_li", 
+                    "a" :""
+                }
+            },
+            {
+                "text": "심리상담",
+                "href": "/mindcare.html",
+                "cls" : {  
+                    "li" : "d2_li", 
+                    "a" :""
+                }
+            }
+        ]
+    },
+    {
+        "text": "커뮤니티",
+        "href": "/community.html",
+        "cls" : {  
+            "li" : " d1_li position-relative ", 
+            "a" :"d1_a",
+            "ul" : " d2 position-absolute ps-0 "
+ },      
+        "d2_li": [
+            {
+                "text": "실시간 후기",
+                "href": "/review.html",
+                "cls" : {  
+                    "li" : "d2_li", 
+                    "a" :""
+                }
+            },
+            {
+                "text": "구독 서비스",
+                "href": "/subscription.html",
+                "cls" : {  
+                    "li" : "d2_li", 
+                    "a" :""
+                }
+            }
+        ]
+    },
+    {
+        "text": "펫트너",
+        "href": "/pettner.html",
+         "cls" : {  
+                    "li" : " d1_li position-relative ", 
+                    "a" :"d1_a",
+                    "ul" : " d2 position-absolute ps-0 "
+         },      
+        "d2_li": [
+            {
+                "text": "펫시터 지원",
+                "href": "/petsitterapply.html",
+                "cls" : {  
+                    "li" : "d2_li", 
+                    "a" :""
+                }
+            },
+            {
+                "text": "훈련사 지원",
+                "href": "/pettrainerapply.html",
+                "cls" : {  
+                    "li" : "d2_li", 
+                    "a" :""
+                }
+            },
+            {
+                "text": "산책케어 지원",
+                "href": "/petwalkapply.html"
+                , "cls" : {  
+                    "li" : "d2_li", 
+                    "a" :""
+                }
+            }
+        ]
+    }
+]
